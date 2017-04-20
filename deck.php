1bbe6147ef57b69f5eb59c806e13a5c3af27fd6f
apekin-pc\apekin <apekin@uamaster.com>
2017-04-20 10:48:19 +0300

    detail deck page

---
+++ deck.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Все о Hearthstone | CardStone</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+<div class="navbar navbar-inverse navbar-fixed-top top-menu "><?php //navbar-static-top ?>
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#main_menu">
+                <span class="sr-only">Open</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a href="/" title="Home" class="navbar-brand hs-logo"><img width="45" src="/img/logo.png"/></a>
+            <span class="logo-text navbar-text">CARDSTONE</span>
+        </div>
+        <div class="hidden-xs col-sm-1"></div>
+        <div class="collapse navbar-collapse" id="main_menu">
+            <ul class="nav navbar-nav museo_title">
+                <li class="active"><a href="#">Новости</a></li>
+                <li><a href="#">Колоды</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Архив <b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">2017</a></li>
+                        <li><a href="#">2016</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Гайды</a></li>
+                <li><a href="#">Карты</a></li>
+            </ul>
+            <form class="navbar-form navbar-right hidden-sm" method="POST">
+                <input type="search" class="search-head" placeholder="Поиск...">&nbsp;<button class="btn btn-link pointer btn-search" type="submit"><i class="fa fa-search"></button></i>
+            </form>
+            <!-- sm search -->
+            <button class="show_search btn btn-link pointer btn-search hidden-md hidden-lg hidden-xs navbar-form navbar-right" type="button">
+                <i class="fa fa-search"></i>
+            </button>
+        </div>
+        <!-- sm search -->
+        <div class="hidden-md hidden-lg hidden-xs hidden-sm" id="compact_search">
+            <form class="navbar-form navbar-right" method="POST" style="width: 100%;">
+                <input type="search" class="search-head " placeholder="Поиск..." style="width: 90%;">&nbsp;<button class="btn btn-link pointer btn-search" type="submit">Найти</button>
+            </form>
+        </div>
+    </div>
+</div>
+
+<div class="container main main-detail">
+    <div class="row white-bg">
+        <div class="col-xs-12 col-md-8">
+            <div class="container col-lg-12" >
+                <h1 class="decks-title">PACTHECOOL’S ARCANE QUEST MAGE (APRIL 2017, SEASON 37)</h1>
+                <div id="deck-master" class="card-list row">
+                    <div class="col-md-6 deck-list">
+                        <div class="deck-header">
+                            <div class="dk-name">
+                                <strong>Классовые (24)</strong>
+                            </div>
+                        </div>
+                        <ul class="deck-class">
+                            <li data-tooltip-img="http://www.hearthstonetopdecks.com/wp-content/uploads/2016/07/babbling-book-1-200x286.png" class="card-frame rare-card ">
+                                <span class="card-cost">1</span>
+                                <span class="card-name">Babbling Book</span>
+                                <span class="card-count">x2</span><span class="card-image card-count-ex"><img src="http://www.hearthstonetopdecks.com/wp-content/uploads/2016/07/sm-babbling-book.png"></span>
+                            </li>
+                            <li data-tooltip-img="http://www.hearthstonetopdecks.com/wp-content/uploads/2014/03/mana-wyrm-200x286.png" class="card-frame rare "><span class="card-cost">1</span><a href="http://www.hearthstonetopdecks.com/cards/mana-wyrm/"><span class="card-name">Mana Wyrm</span></a><span class="card-count">2</span><span class="card-image card-count-ex"><img src="http://www.hearthstonetopdecks.com/wp-content/uploads/2014/03/sm-card-mana-wyrm.png"></span>
+                            </li>
+                            <li data-tooltip-img="http://www.hearthstonetopdecks.com/wp-content/uploads/2017/03/open-the-waygate-200x286.png" class="card-frame rare-0 "><span class="card-cost">1</span><a href="http://www.hearthstonetopdecks.com/cards/open-the-waygate/"><span class="card-name">Open the Waygate</span></a><span class="card-count">1</span><span class="card-image card-count-ex"><img src="http://www.hearthstonetopdecks.com/wp-content/uploads/2017/03/sm-open-the-waygate.png"></span>
+                            </li><li data-tooltip-img="http://www.hearthstonetopdecks.com/wp-content/uploads/2017/03/arcanologist-200x286.png" class="card-frame rare-1 "><span class="card-cost">2</span><a href="http://www.hearthstonetopdecks.com/cards/arcanologist/"><span class="card-name">Arcanologist</span></a><span class="card-count">2</span><span class="card-image card-count-ex"><img src="http://www.hearthstonetopdecks.com/wp-content/uploads/2017/03/sm-arcanologist.png"></span>
+                            </li><li data-tooltip-img="http://www.hearthstonetopdecks.com/wp-content/uploads/2014/03/frostbolt-200x286.png" class="card-frame rare-2 "><span class="card-cost">2</span><a href="http://www.hearthstonetopdecks.com/cards/frostbolt/"><span class="card-name">Frostbolt</span></a><span class="card-count">2</span><span class="card-image card-count-ex"><img src="http://www.hearthstonetopdecks.com/wp-content/uploads/2014/03/sm-card-frostbolt.png"></span>
+                            </li><li data-tooltip-img="http://www.hearthstonetopdecks.com/wp-content/uploads/2016/08/medivhs-valet-200x286.png" class="card-frame rare-3 "><span class="card-cost">2</span><a href="http://www.hearthstonetopdecks.com/cards/medivhs-valet/"><span class="card-name">Medivh’s Valet</span></a><span class="card-count">2</span><span class="card-image card-count-ex"><img src="http://www.hearthstonetopdecks.com/wp-content/uploads/2016/08/sm-card-medivhs-valet.png"></span>
+                            </li><li data-tooltip-img="http://www.hearthstonetopdecks.com/wp-content/uploads/2017/03/primordial-glyph-200x286.png" class="card-frame epic-card "><span class="card-cost">2</span><a href="http://www.hearthstonetopdecks.com/cards/primordial-glyph/"><span class="card-name">Primordial Glyph</span></a><span class="card-count">2</span><span class="card-image card-count-ex"><img src="http://www.hearthstonetopdecks.com/wp-content/uploads/2017/03/sm-primordial-glyph.png"></span>
+                            </li><li data-tooltip-img="http://www.hearthstonetopdecks.com/wp-content/uploads/2017/03/shimmering-tempest-200x286.png" class="card-frame common-card "><span class="card-cost">2</span><a href="http://www.hearthstonetopdecks.com/cards/shimmering-tempest/"><span class="card-name">Shimmering Tempest</span></a><span class="card-count">2</span><span class="card-image card-count-ex"><img src="http://www.hearthstonetopdecks.com/wp-content/uploads/2017/03/sm-shimmering-tempest.png"></span>
+                            </li><li data-tooltip-img="http://www.hearthstonetopdecks.com/wp-content/uploads/2014/03/arcane-intellect-200x286.png" class="card-frame free-card "><span class="card-cost">3</span><a href="http://www.hearthstonetopdecks.com/cards/arcane-intellect/"><span class="card-name">Arcane Intellect</span></a><span class="card-count">2</span><span class="card-image card-count-ex"><img src="http://www.hearthstonetopdecks.com/wp-content/uploads/2014/03/sm-card-arcane-intellect.png"></span>
+                            </li><li data-tooltip-img="http://www.hearthstonetopdecks.com/wp-content/uploads/2014/03/frost-nova-200x286.png" class="card-frame free-card "><span class="card-cost">3</span><a href="http://www.hearthstonetopdecks.com/cards/frost-nova/"><span class="card-name">Frost Nova</span></a><span class="card-count">2</span><span class="card-image card-count-ex"><img src="http://www.hearthstonetopdecks.com/wp-content/uploads/2014/03/sm-card-frost-nova.png"></span>
+                            </li><li data-tooltip-img="http://www.hearthstonetopdecks.com/wp-content/uploads/2014/03/ice-block-200x286.png" class="card-frame epic-card "><span class="card-cost">3</span><a href="http://www.hearthstonetopdecks.com/cards/ice-block/"><span class="card-name">Ice Block</span></a><span class="card-count">2</span><span class="card-image card-count-ex"><img src="http://www.hearthstonetopdecks.com/wp-content/uploads/2014/03/sm-card-ice-block.png"></span>
+                            </li><li data-tooltip-img="http://www.hearthstonetopdecks.com/wp-content/uploads/2014/03/fireball-200x286.png" class="card-frame free-card "><span class="card-cost">4</span><a href="http://www.hearthstonetopdecks.com/cards/fireball/"><span class="card-name">Fireball</span></a><span class="card-count">2</span><span class="card-image card-count-ex"><img src="http://www.hearthstonetopdecks.com/wp-content/uploads/2014/03/sm-card-fireball.png"></span>
+                            </li><li data-tooltip-img="http://www.hearthstonetopdecks.com/wp-content/uploads/2016/03/cabalists-tome-hd-200x286.png" class="card-frame epic-card "><span class="card-cost">5</span><a href="http://www.hearthstonetopdecks.com/cards/cabalists-tome/"><span class="card-name">Cabalist’s Tome</span></a><span class="card-count">1</span><span class="card-image card-count-ex"><img src="http://www.hearthstonetopdecks.com/wp-content/uploads/2016/03/sm-card_cabalists-tome.png"></span>
+                            </li>						</ul>
+                    </div>
+
+                    <div class="col-md-6 deck-list">
+                        <div class="deck-header">
+                            <div class="dk-name">
+                                <strong>Общие (6)</strong>
+                            </div>
+                        </div>
+                        <ul class="deck-class neutral-cards">
+                            <li data-tooltip-img="http://www.hearthstonetopdecks.com/wp-content/uploads/2017/03/fire-fly-200x286.png" class="card-frame common-card "><span class="card-cost">1</span><a href="http://www.hearthstonetopdecks.com/cards/fire-fly/"><span class="card-name">Fire Fly</span></a><span class="card-count">2</span><span class="card-image card-count-ex"><img src="http://www.hearthstonetopdecks.com/wp-content/uploads/2017/03/sm-fire-fly-1.png"></span>
+                            </li><li data-tooltip-img="http://www.hearthstonetopdecks.com/wp-content/uploads/2016/11/kabal-courier-1-200x286.png" class="card-frame rare-card "><span class="card-cost">3</span><a href="http://www.hearthstonetopdecks.com/cards/kabal-courier/"><span class="card-name">Kabal Courier</span></a><span class="card-count">2</span><span class="card-image card-count-ex"><img src="http://www.hearthstonetopdecks.com/wp-content/uploads/2016/11/sm-kabal-courier-1.png"></span>
+                            </li><li data-tooltip-img="http://www.hearthstonetopdecks.com/wp-content/uploads/2016/08/arcane-giant-200x286.png" class="card-frame epic-card "><span class="card-cost">12</span><a href="http://www.hearthstonetopdecks.com/cards/arcane-giant/"><span class="card-name">Arcane Giant</span></a><span class="card-count">2</span><span class="card-image card-count-ex"><img src="http://www.hearthstonetopdecks.com/wp-content/uploads/2016/08/sm-card-arcane-giant.png"></span>
+                            </li>						</ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4"></div>
+    </div>
+</div>
+
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!-- Latest compiled and minified JavaScript -->
+<script src="/js/bootstrap.min.js"></script>
+<script src="/js/custom.js"></script>
+</body>
+</html>
